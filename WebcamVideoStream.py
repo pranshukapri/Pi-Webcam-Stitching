67229e2e33c0ea0fa83a7bcccb543f1fac5063e6
pranshukapri <pranshukapri@gmail.com>
2020-10-01 12:43:08 +0530

    Added python files for realtime stitching

---
+++ WebcamVideoStream.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 19:12:39 2019
+
+@author: prans
+"""
+
+# import the necessary packages
+from threading import Thread
+import cv2
+ 
+class WebcamVideoStream:
+	def __init__(self, src=0):
+		# initialize the video camera stream and read the first frame
+		# from the stream
+		self.stream = cv2.VideoCapture(src)
+		(self.grabbed, self.frame) = self.stream.read()
+ 
+		# initialize the variable used to indicate if the thread should
+		# be stopped
+		self.stopped = False
+        def start(self):
+		# start the thread to read frames from the video stream
+		Thread(target=self.update, args=()).start()
+		return self
+ 
+	def update(self):
+		# keep looping infinitely until the thread is stopped
+		while True:
+			# if the thread indicator variable is set, stop the thread
+			if self.stopped:
+				return
+ 
+			# otherwise, read the next frame from the stream
+			(self.grabbed, self.frame) = self.stream.read()
+ 
+	def read(self):
+		# return the frame most recently read
+		return self.frame
+ 
+	def stop(self):
+		# indicate that the thread should be stopped
+		self.stopped = True
